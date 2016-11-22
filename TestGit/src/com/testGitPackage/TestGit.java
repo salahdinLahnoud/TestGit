98e7596547f15d6010a6b68e62abb03fd00aec42
Salahdin Lahnoud <salahdin.lahnoud@gmail.com>
2016-11-22 12:16:47 +0100

    committed Test Git project

---
+++ TestGit/src/com/testGitPackage/TestGit.java
@@ -0,0 +1,11 @@
+package com.testGitPackage;
+
+public class TestGit {
+	public static void main(String []args){
+		
+		System.out.println("Test Git!!!!");
+		System.out.println("Test Git!!!!");
+		
+	}
+
+}
