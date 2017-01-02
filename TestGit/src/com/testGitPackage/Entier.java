7a4a0e52db77eb4aa2b62d18ca7037695303ca3b
Salahdin Lahnoud <salahdin.lahnoud@gmail.com>
2017-01-02 21:39:17 +0100

    co

--- TestGit/src/com/testGitPackage/Entier.java
+++ TestGit/src/com/testGitPackage/Entier.java
@@ -2,4 +2,19 @@ package com.testGitPackage;
 
 public class Entier {
 
+	
+	
+	int x;
+	public Entier(int x){
+		this.x = x;
+		
+	}
+	
+
+	public void print(){
+		
+		System.out.println(this.x);
+	}
+	
+	
 }
